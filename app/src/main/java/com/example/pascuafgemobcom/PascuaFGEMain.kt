d93cff034b528a46b18a7b45862f1a11d3fb7830
julynpascua <20094800@s.ubaguio.edu>
2021-09-15 11:57:33 +0800

    first grading exam

--- app/src/main/java/com/example/pascuafgemobcom/PascuaFGEMain.kt
+++ app/src/main/java/com/example/pascuafgemobcom/PascuaFGEMain.kt
@@ -1,5 +1,70 @@
 package com.example.pascuafgemobcom
 
-fun main(){
-    
+fun main() {
+    print("Enter first number: ")
+    var num1 = readLine()!!.toDouble()
+    do {
+        print("Enter operator: ")
+        var oper = readLine()
+
+        if (oper == "+") {
+           num1=sum(num1)
+        }
+        else if(oper=="-"){
+            num1=diff(num1)
+        }
+        else if(oper=="*"){
+            num1=prod(num1)
+        }
+        else if(oper=="/"){
+            num1=quot(num1)
+        }
+        else if(oper=="="){
+            println("Result of  computation is $num1")
+        }
+    } while (oper != "=")
+
 }
+
+//addition of inputs
+fun sum(num1:Double):Double{
+    print("Next number: ")
+    var num2=readLine()!!.toDouble()
+    var sum1=num1+num2
+    return sum1
+}
+
+//subtraction of inputs
+fun diff(num1:Double):Double{
+    print("Next number: ")
+    var num2=readLine()!!.toDouble()
+    var diff1=num1-num2
+    return diff1
+}
+
+//multiplication of inputs
+fun prod(num1:Double):Double{
+    print("Next number: ")
+    var num2=readLine()!!.toDouble()
+    var prod1=num1*num2
+    return prod1
+}
+
+//division of inputs
+fun quot(num1:Double):Double{
+    print("Next number: ")
+    var num2=readLine()!!.toDouble()
+    if(num2!=0.0){ //if nonzero
+        var quot1 = num1 / num2
+        return quot1
+    }
+    else{ //if zero then back to ask operation
+        println("Invalid division with zero!")
+        return num1
+    }
+
+}
+
+
+
+
